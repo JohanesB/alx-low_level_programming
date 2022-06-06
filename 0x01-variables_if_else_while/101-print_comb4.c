4ccc30d21df6f85f055aa06369460fb7a6cbf44d
Yohanes <yohanesbacha@gmail.com>
2022-06-06 13:32:26 -0700

    commit 12 for task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/***
+/**
  * main - Entry point
  *
  * Return: Always 0(Access)
@@ -12,15 +12,15 @@ int main(void)
 
 	for (x = 0; x < 8; x++)
 	{
-		for (y = 0; y < 9; y++)
+		for (y = x+1; y < 9; y++)
 		{
-			for (z = 0; z < 10; z++)
+			for (z = y+1; z < 10; z++)
 			{
 				putchar((x % 10) + '0');
 				putchar((y % 10) + '0');
 				putchar((z % 10) + '0');
 
-				if (x == 8 && y == 9 && z == 10)
+				if (x == 7 && y == 8 && z == 9)
 					continue;
 				putchar(',');
 				putchar(' ');

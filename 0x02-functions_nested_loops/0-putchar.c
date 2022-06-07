8797597e3509206084897ffdeac7f7b94659e3db
Yohanes <yohanesbacha@gmail.com>
2022-06-07 11:22:37 -0700

    Initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,11 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <main.h>
 /**
  * main - Entry point
  *
  * Return: Always 0(Success)
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 int main(void)
 {
 	char word[8] = "_putchar";

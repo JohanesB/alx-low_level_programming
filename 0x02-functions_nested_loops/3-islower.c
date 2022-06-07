12d99d4054d9444ad1063f26bd0309cb6f22e32f
Yohanes <yohanesbacha@gmail.com>
2022-06-07 14:06:43 -0700

    commit 4 for task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
  * islower - checks for lowercase character
  * @c: character to check
  * alphabet in lowercase 10 times
+ * Description: function uses _putchar function to print
  * Return: 1 if lowercase, 0 if uppercase
  */
 int _islower(int c)

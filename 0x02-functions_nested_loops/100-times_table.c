6aae50451b42b66cfb5891da6665c2399e9f24ae
Yohanes <yohanesbacha@gmail.com>
2022-06-08 11:34:23 -0700

    commit 13 for task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,12 +7,12 @@
 void print_times_table(int n)
 {
 	int x, y, z;
+
 	if (n >= 0 && n <= 14)
 	{
-
 		for (x = 0; x <= n; x++)
 		{
-			for (y = 0; y <= n; y+++)
+			for (y = 0; y <= n; y++)
 			{
 				z = x * y;
 				if (z > 99)

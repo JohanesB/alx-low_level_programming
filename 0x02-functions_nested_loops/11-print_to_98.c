7e67ac4d1aea5f415ae22d6b1e009c830512f849
Yohanes <yohanesbacha@gmail.com>
2022-06-08 10:12:28 -0700

    commit 12 for task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,10 +10,10 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n <= 89; n++)
+		for (; n <= 98; n++)
 		{
 			printf("%d", n);
-			if (n == 89)
+			if (n == 98)
 				continue;
 			printf(",");
 			printf(" ");
@@ -22,10 +22,10 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (; n >= 89; n--)
+		for (; n >= 98; n--)
 		{
 			printf("%d", n);
-			if (n == 89)
+			if (n == 98)
 				continue;
 			printf(",");
 			printf(" ");

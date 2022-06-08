b6849c17513d6c4d9c32b58345d26cfcd3d55284
Yohanes <yohanesbacha@gmail.com>
2022-06-08 12:16:25 -0700

    commit 15 for task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,19 +7,19 @@
 int main(void)
 {
 	int i;
-	int fibonacci[50];
+	long int fibonacci[50];
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
-	printf("%d, %d", fibonacci[0], fibonacci[1]);
+	printf("%1d, %1d,", fibonacci[0], fibonacci[1]);
 
 	for (i = 2; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 		if (i != 49)
-			printf("%d, ", fibonacci[i]);
+			printf("%1d, ", fibonacci[i]);
 		else
-			printf("%d\n", fibonacci[i]);
+			printf("%1d\n", fibonacci[i]);
 	}
 	return (0);
 }

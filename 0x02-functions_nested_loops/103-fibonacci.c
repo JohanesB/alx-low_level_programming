2cfeb36a81c8ec849dda93b108a1160ab1d40754
Yohanes <yohanesbacha@gmail.com>
2022-06-08 12:35:20 -0700

    commit 16 for task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -15,8 +15,8 @@ int main(void)
 	for (i = 2; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		if ((fibonacci[i] / 2) == 0 && fibonacci[i] < 4000000)
-			sum +=fibonacci[i];
+		if ((fibonacci[i] % 2) == 0 && fibonacci[i] < 4000000)
+			sum += fibonacci[i];
 	}
 	printf("%ld\n", sum);
 	return (0);

3fd8a082ec7b72c31c9584fb44221c70038b61bc
Yohanes <yohanesbacha@gmail.com>
2022-06-08 09:06:17 -0700

    commit 9 for task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day of Jack Bauer
  * Description: You can listen to this soundtrack while coding
  * Return: Nothing
  */

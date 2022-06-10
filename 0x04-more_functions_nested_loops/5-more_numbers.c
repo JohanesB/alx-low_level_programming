b914a6ebf005b98ecf123a1f0c892617bce6fd93
Yohanes <yohanesbacha@gmail.com>
2022-06-10 09:17:18 -0700

    commit 6 for task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.c"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: Nothing
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+			_putchar(j + '0');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-task 5: a function that prints the numbers, from 0 to 9 except 2 and 4.
+task 5:a function that prints 10 times the numbers, from 0 to 14, followed by a new line

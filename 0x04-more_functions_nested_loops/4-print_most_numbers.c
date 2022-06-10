389cd305565792e2f4d1594c85ecdf1ffe0a7858
Yohanes <yohanesbacha@gmail.com>
2022-06-10 08:10:09 -0700

    commit 5 for task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,10 +8,11 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 19; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		if (i == 2 || i == 4)
 			continue;
-		return (i)
+		_putchar(i + '0');
 	}
+	_putchar('\n');
 }	

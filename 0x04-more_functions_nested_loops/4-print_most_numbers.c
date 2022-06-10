50dca8b0426dfe09b68297cdbabe182c1d40f496
Yohanes <yohanesbacha@gmail.com>
2022-06-10 08:12:00 -0700

    commit 5 for task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,6 +3,7 @@
 /**
  * print_most_numbers - function that prints the numbers, from 0 to 9
  * Return: Nothing
+ * Description: Prints the numbers excluding 2 and 4
  */
 void print_most_numbers(void)
 {

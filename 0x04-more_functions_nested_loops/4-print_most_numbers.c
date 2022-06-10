115b9ae69e57bfd89da6372a48837e7864d4fdff
Yohanes <yohanesbacha@gmail.com>
2022-06-10 08:05:57 -0700

    commit 5 for task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9
+ * Return: Nothing
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 19; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		return (i)
+	}
+}	
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-task 3: a function that prints the numbers, from 0 to 9, followed by a new line.
+task 5: a function that prints the numbers, from 0 to 9 except 2 and 4.

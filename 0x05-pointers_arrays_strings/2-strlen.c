d5a7ce42e65b3ee033cc0a589b8c37a678f68300
Yohanes <yohanesbacha@gmail.com>
2022-06-13 11:04:30 -0700

    commit 3 for task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _strlen - a function that returns the length of a string.
- * @s: pointer to point a character
+ * @s: String to count
  * Return: integer
  */
 int _strlen(char *s)
 {
-	int i;
+	int i = 0;
 
 	for (; *s != '\0'; s++)
 		i++;

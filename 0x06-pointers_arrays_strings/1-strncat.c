af3a44ed73384b50ed028d3fb66e18dace25a662
Yohanes <yohanesbacha@gmail.com>
2022-06-16 04:19:02 -0700

    commit 2 for task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,25 @@
 #include "main.h"
 /**
  * _strncat - a function that concatenates two strings.
- * @dest: string to copy
- * @src: string being copied
- * @n: largest number of bytes to copy
+ * @dest: string to append by src
+ * @src: string to append to dest
+ * @n: largest number of bytes to append
  * Return: address of dust
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
+	int j = 0;
 
-	while (i < n && *(src + i))
-	{
-		*(dest + i) = *(src + i);
+	while (*(dest + i))
 		i++;
-	}
-	while (i < n)
+	while (j > n && *(src + i))
 	{
-		*(dest + i) = '\0';
-		i++
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
 	}
+	if (j < n)
+		*(dest + i) = *(src + j);
 	return (dest);
 }

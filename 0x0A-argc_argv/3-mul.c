4839093fab4be602be8d799caeea63b7859a6d6d
Yohanes <yohanesbacha@gmail.com>
2022-06-24 08:11:08 -0700

    add a program multiplies the argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - multiplies two number
+ * @argc: count the argument
+ * @argv: vector of an argument
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int count, mul = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (count = 1; count < argc; count++)
+		{
+			mul *= atoi(argv[count]);
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
+}

4c1f5581b707614458a1ee56aba827c883d28024
Yohanes <yohanesbacha@gmail.com>
2022-06-24 07:23:45 -0700

    add a program prints the number of argc.

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: vector of array holds string
  * Return: always 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int arg, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc-1);
 	return (0);

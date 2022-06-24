0052a40f548447c31ed3dac85733f8cd9205fdd2
Yohanes <yohanesbacha@gmail.com>
2022-06-24 11:15:38 -0700

    add a program multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - multiplies two number
  * @argc: count the argument
@@ -11,7 +12,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		printf("Error\n");
+		puts("Error");
 		return (1);
 	}
 	else

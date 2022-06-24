d5bca54572429fc5aac52750dba3524fd44fe56d
Yohanes <yohanesbacha@gmail.com>
2022-06-24 11:51:18 -0700

    add a program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - adds positive numbers
  * @argc: count an argument
@@ -24,7 +24,7 @@ int main (int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		a += atoi(argv[i]);
+		a = a + atoi(argv[i]);
 	}
 	printf("%d\n", a);
 	return (0);

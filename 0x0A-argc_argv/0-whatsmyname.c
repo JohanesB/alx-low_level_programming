17e1aca745ce60951be9be1ebf70548f96b9c529
Yohanes <yohanesbacha@gmail.com>
2022-06-24 06:04:55 -0700

    Initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - print the name of a program
+ * @argc: count the argument
+ * @argv: array of the string
+ * Return: 0
+ */
+int main(int argc, char* argv[])
+{
+	_putchar("%s \n", argv[0]);
+	return (0);
+}

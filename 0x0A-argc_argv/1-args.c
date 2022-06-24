911e11d7935d0b03ad075b587e5c8e7d7d78ed95
Yohanes <yohanesbacha@gmail.com>
2022-06-24 07:10:31 -0700

    Adds a program print a number of argumemnts

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments
+ * @argc: count the argument
+ * @argv: vector of array holds string
+ * Return: always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", argc);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,2 @@
 Task 0: a program that prints its name, followed by a new line.
+Task 1: a program that prints the number of arguments passed into it.

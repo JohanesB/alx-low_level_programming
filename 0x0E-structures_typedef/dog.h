e0dcae20862905f6ee86e3e2f2efa98c09adc698
Yohanes <yohanesbacha@gmail.com>
2022-07-04 13:48:38 -0700

    Initial commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef _ALX_
 #define _ALX_
 
+/**
+ * struct dog - information about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 #endif /* _ALX_ */

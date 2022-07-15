86d4aba5ab4982eeaf56466e2f98844f38021e98
Yohanes <yohanesbacha@gmail.com>
2022-07-15 14:07:31 -0700

    adds a new node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,10 @@
 #ifndef _ALX_
 #define _ALX_
 
+#include <stddef.h>
+
 /**
- * struct list_s -i singly linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
@@ -11,15 +13,14 @@
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
-int _putchar(char);
+int _strlen(const char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-int _strlen(const char *s);
 
-#endif /* _AlX_ */
+#endif

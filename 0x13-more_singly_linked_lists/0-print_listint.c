cf52607b54a79fdf870ab42093d645c6a675ff7f
Yohanes <yohanesbacha@gmail.com>
2022-07-19 14:08:37 -0700

    print all element

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,11 +2,11 @@
 #include "lists.h"
 
 /**
-  * print_listint - Prints all the elements of a linked list
-  * @h: The head of the linked list
-  *
-  * Return: The number of nodes of the linked list
-  */
+ * print_listint -  prints all the elements of a listint_t list.
+ * @h: list to print
+ *
+ * Return: number of elements in the list
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
